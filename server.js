f77993d0588995d78e189b3e2a0bcf049a7c8e5a
AyushKumarMishra5 <ayushkmishra22aug@gmail.com>
2025-04-10 07:49:37 +0530

    Database connected and data successfully added to it

--- server.js
+++ server.js
@@ -3,13 +3,18 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const { v4: uuidV4 } = require('uuid');
+const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
+app.get('/signup', (req, res)=>{
+  res.sendFile(path.join(__dirname, 'public', 'sgnup.html'))
+})
 
 app.get('/room/:room', (req, res) => {
   res.render('room', { roomId: req.params.room });
@@ -28,8 +33,13 @@ io.on('connection', socket => {
   });
 });
 
+const MongoDB_URI = process.env.MongoDB_URI
+mongoose.connect("mongodb://localhost:27017/video")
+.then(()=> console.log("Connected to the Database."))
+.catch((err)=> console.log("Error connecting to the database", err))
+
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
 
